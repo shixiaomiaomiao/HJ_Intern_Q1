fc2d5956930514cc680eaa14029400224a659ace
shixiaomiaomiao <shijiale_ecnu@139.com>
2015-09-24 20:40:11 +0800

    q2_1 && q2_2

---
+++ q2_2.js
@@ -0,0 +1,49 @@
+var topBar = document.getElementById("topBar");
+var container = getByClass(topBar,"container")[0];
+var a = container.getElementsByTagName('a')[0];
+var logoImg = container.getElementsByTagName('img')[0];
+a.setAttribute("href","javascript:;");
+var eleArr = document.getElementsByTagName("img");
+logoImg.addEventListener("click", function(){
+	for ( var i = 0 ; i < eleArr.length ; i++ )
+	{
+		setTimer(eleArr[i]);
+	}
+},false)
+
+function getByClass( oParent, sClass )
+{
+	var aResult = [];
+	var aEle = oParent.getElementsByTagName("*");
+	for ( var i = 0; i < aEle.length ; i++ )
+	{
+		if ( aEle[i].className == sClass )
+		{
+			aResult.push(aEle[i]);
+		}
+	}
+	return aResult;
+}
+function setTimer( obj )
+{
+	console.log( obj);
+	clearInterval(obj.timer);
+	obj.n = 0;
+	obj.ny = 0;
+	obj.timer = setInterval(function(){
+		obj.ny += 1;
+		obj.style.transform = "rotateY(" + obj.ny + "deg)";
+		obj.style.webkitTransform = "rotateY(" + obj.ny + "deg)";
+		obj.style.OTransform = "rotateY(" + obj.ny + "deg)";
+		obj.style.MozTransform = "rotateY(" + obj.ny + "deg)";
+		if (obj.ny == 180 || obj.ny >= 360)
+		{
+			console.log(obj.ny);
+			clearInterval( obj.timer );
+			if ( obj.ny >= 360 ) 
+			{
+				obj.ny = 0;
+			}
+		}
+	},2);
+}
